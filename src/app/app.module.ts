da55f4855bb8c07d87cebf67054dcbe357013acb
Anlanther <25150182+Anlanther@users.noreply.github.com>
2022-03-21 00:02:57 +0800

    preparing for routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TaskListComponent } from './task/task-list/task-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TaskListComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule, // For 2 way binding. Will use for filtering
     AppRoutingModule
   ],
   providers: [],
