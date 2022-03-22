a29230e3c081af6afc26f750a151615bad9c0cdc
Anlanther <25150182+Anlanther@users.noreply.github.com>
2022-03-22 13:42:06 +0800

    created new task components

--- src/app/task/task-list/task-list.component.ts
+++ src/app/task/task-list/task-list.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { ITask } from '../task';
+import { TaskService } from '../task.service';
 
 @Component({
-  selector: 'app-task-list',
+  // selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss']
 })
 export class TaskListComponent implements OnInit {
   filteredTasks: ITask[] = [];
   
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
   }
