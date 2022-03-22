e02fbfccad3aab6df81de515120a7343fbce0f66
Anlanther <25150182+Anlanther@users.noreply.github.com>
2022-03-22 13:41:35 +0800

    added routing for new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,38 @@ import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
 import { HomeComponent } from './home/home.component';
 import { TaskListComponent } from './task/task-list/task-list.component';
+import { TaskDetailComponent } from './task/task-detail/task-detail.component';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { TaskData } from './task/task-data';
+import { CompletedListComponent } from './task/completed-list/completed-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    TaskListComponent
+    TaskListComponent,
+    HeaderBarComponent,
+    TaskDetailComponent,
+    CompletedListComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(TaskData, { delay: 1000 }), // creating a delay to mimic db behaviour
     FormsModule, // For 2 way binding. Will use for filtering
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'tasks', component: TaskListComponent },
+      { path: 'tasks/:id', component: TaskDetailComponent },
+      { path: 'completed', component: CompletedListComponent },
+      { path: 'home', component: HomeComponent },
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: '**', redirectTo: 'home', pathMatch: 'full' }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
